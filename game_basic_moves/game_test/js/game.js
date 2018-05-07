4f9194dcc3636bea7530c0248e42627e2475bdfb
EC2 Default User <ec2-user@ip-172-31-9-91.us-east-2.compute.internal>
2018-05-07 21:49:14 +0000

    sprites and selecting added

--- game_basic_moves/game_test/js/game.js
+++ game_basic_moves/game_test/js/game.js
@@ -12,7 +12,7 @@ var game = {
     // Run on page load.
     "onload" : function () {
         // Initialize the video.
-        if (!me.video.init(1024, 1024, {wrapper : "screen", scale : "auto"})) {
+        if (!me.video.init(640, 480, {wrapper : "screen", scale : "auto"})) {
             alert("Your browser does not support HTML5 canvas.");
             return;
         }
@@ -31,14 +31,17 @@ var game = {
         me.state.set(me.state.PLAY, new game.PlayScreen());
 
         // add our player entity in the entity pool
-        //me.pool.register("mainPlayer", game.PlayerEntity);
         me.pool.register("testKnight", game.Knight);
-        me.pool.register("testVillian", game.Villian);
+        me.pool.register("testVillain", game.Villain);
+        me.pool.register("testCavalry", game.Cavalry);
+        me.pool.register("testArcher", game.Archer);
+        me.pool.register("selectbox", game.selectbox);
+        me.pool.register("unitSelected", game.unitSelected);
 
         // Enable for testing
         me.debug.renderHitBox = true
 
-        me.input.pointer.RIGHT.preventDefault;
+
         // Start the game.
         me.state.change(me.state.PLAY);
 
--- game_basic_moves/game_test/js/screens/play.js
+++ game_basic_moves/game_test/js/screens/play.js
@@ -9,29 +9,39 @@ game.PlayScreen = me.ScreenObject.extend({
         // reset the score
         game.data.score = 0;
         
-        game.Knight = new game.Knight();
+
+        game.selectbox = new game.selectbox();
+
 
         // Add our HUD to the game world, add it last so that this is on top of the rest.
         // Can also be forced by specifying a "Infinity" z value to the addChild function.
         this.HUD = new game.HUD.Container();
         me.game.world.addChild(this.HUD);
 
-        me.game.world.addChild(me.pool.pull("testKnight"));
-        me.game.world.addChild(me.pool.pull("testVillian"));
-        //me.game.world.addChild(me.pool.pull("testKnight"),100,100);
+        me.game.world.addChild(me.pool.pull("testKnight", 0, 0));
+        me.game.world.addChild(me.pool.pull("testKnight", 20, 20));
+        me.game.world.addChild(me.pool.pull("testCavalry", 60, 20));
+        me.game.world.addChild(me.pool.pull("testArcher", 100, 20));
+        me.game.world.addChild(me.pool.pull("testVillain"));
+        me.game.world.addChild(me.pool.pull("selectbox"));
+        me.game.world.addChild(me.pool.pull("unitSelected"));
 
+
+
+/*
         //bind arrow keys
         me.input.bindKey(me.input.KEY.LEFT, 'left');
         me.input.bindKey(me.input.KEY.RIGHT, 'right');
         me.input.bindKey(me.input.KEY.UP, 'up');
         me.input.bindKey(me.input.KEY.DOWN, 'down'); 
+*/
 
         //bind clicks
         me.input.bindKey(me.input.KEY.M, 'leftclick'); 
         me.input.bindPointer(me.input.KEY.M);
-		me.input.bindKey(me.input.KEY.A, 'rightclick'); 
-        me.input.bindPointer(me.input.pointer.RIGHT, me.input.KEY.A, false, false);
-
+		me.input.bindKey(me.input.KEY.A, 'rightclick');
+		me.input.bindPointer(me.input.pointer.RIGHT, me.input.KEY.A);
+        //me.input.registerPointerEvent('pointerdown', me.game.viewport, this.pointerDown.bind(this));
     },
 
     /**
@@ -41,4 +51,11 @@ game.PlayScreen = me.ScreenObject.extend({
         // remove the HUD from the game world
         me.game.world.removeChild(this.HUD);
     }
+});
+
+
+
+//prevent default right-click menu from popping up
+document.addEventListener("contextmenu", function(e) {
+  e.preventDefault();
 });
